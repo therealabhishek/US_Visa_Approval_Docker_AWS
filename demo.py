74e5a72c28541773b1dfe3647e5ffbcf8928de16
Abhishek Kulkarni <kul.abhishake@gmail.com>
2024-07-29 12:01:37 +0530

    data validation completed

--- demo.py
+++ demo.py
@@ -1,12 +1,17 @@
-#from us_visa.configuration.mongo_db_connection import MongoDBClient
+from us_visa.configuration.mongo_db_connection import MongoDBClient
 
-#ins = MongoDBClient()
+ins = MongoDBClient()
 
-from us_visa.entity.config_entity import DataIngestionConfig
-#from us_visa.entity.artifact_entity import DataIngestionArtifact
+from us_visa.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from us_visa.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 
 from us_visa.components.data_ingestion import DataIngestion
+from us_visa.components.data_validation import DataValidation
 
 di_ins = DataIngestion(DataIngestionConfig)
 
 di_artifact = di_ins.initiate_data_ingestion()
+
+dv_ins = DataValidation(data_ingestion_artifact=di_artifact, data_validation_config=DataValidationConfig)
+
+dv_artifact = dv_ins.initiate_data_validation()
